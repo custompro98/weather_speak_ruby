f57a2cdb383dcab914938fcfca9d119f873ebf8a
Mitch <mitchjoa@gmail.com>
2015-05-09 14:11:02 -0400

    Configuration setup

---
+++ weather_speak.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'open-uri'
+require 'pp'
+
+class Configuration
+	@@config = {}
+
+	def self.set(api_key, zip_code)
+		@@config["api_key"] = api_key
+		@@config["zip_code"] = zip_code
+	end
+
+	def self.get(property_name)
+		@@config[property_name]
+	end
+end
+
+def init
+	config_path = ENV["HOME"] + "/.weatherspeak"
+	if File.exist?(config_path)
+		f = File.open(config_path, "r")
+		api_key = f.gets
+		zip_code = ARGV[0]
+		if zip_code.nil?
+			zip_code = f.gets
+		end
+		f.close
+	else
+		puts "worldweatheronline API key: "
+		api_key = STDIN.gets
+		zip_code = ARGV[0]
+		if zip_code.nil?
+			zip_code = "48067"
+		end
+
+		f = File.open(config_path, "w")
+		f.puts api_key
+		f.puts zip_code
+		f.close
+	end
+
+	Configuration.set(api_key, zip_code)
+end
+
+def main
+	init
+	base_uri = 'http://api.worldweatheronline.com/free/v2/weather.ashx?'
+	params = Hash.new
+
+	puts Configuration.get("api_key")
+	puts Configuration.get("zip_code")
+end
+
+main
+
+# if test.nil?
+# 	puts "It was nil!"
+# else
+# 	puts test
+# end
+
+# resp = open(url).read
+
+# result = JSON.parse(resp)
+# puts result
+# test = "boop"
+# `say #{test}`
