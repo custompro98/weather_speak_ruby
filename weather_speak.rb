6a37cad934130ad3ea3ebbf9b9c4bf625a5f2859
Mitch <mitchjoa@gmail.com>
2015-05-09 17:11:13 -0400

    URI parsing, added speech output, using espeak

--- weather_speak.rb
+++ weather_speak.rb
@@ -4,12 +4,17 @@ require 'json'
 require 'open-uri'
 require 'pp'
 
+require 'espeak'
+
+include ESpeak
+
 class Configuration
 	@@config = {}
 
 	def self.set(api_key, zip_code)
 		@@config["api_key"] = api_key
 		@@config["zip_code"] = zip_code
+		@@config["format"] = "json"
 	end
 
 	def self.get(property_name)
@@ -48,8 +53,27 @@ def main
 	init
 	base_uri = 'http://api.worldweatheronline.com/free/v2/weather.ashx?'
 
+	direction = Hash.new
+	direction["N"] = "north"
+	direction["NNE"] = "north northeast"
+	direction["NE"] = "northeast"
+	direction["ENE"] = "east northeast"
+	direction["E"] = "east"
+	direction["ESE"] = "east southeast"
+	direction["SE"] = "southeast"
+	direction["SSE"] = "south southeast"
+	direction["S"] = "south"
+	direction["SSW"] = "south southwest"
+	direction["SW"] = "southwest"
+	direction["WSW"] = "west southwest"
+	direction["W"] = "west"
+	direction["WNW"] = "west northwest"
+	direction["NW"] = "northwest"
+	direction["NNW"] = "north northwest"
+
 	params = Hash.new
 	params["key"] = Configuration.get("api_key")
+	params["format"] = Configuration.get("format")
 	params["q"] = Configuration.get("zip_code")
 
 	uri = base_uri
@@ -61,20 +85,22 @@ def main
 		end
 	end
 
-	puts uri
+	resp = open(uri).read
+	result = JSON.parse(resp)
+
+	feels_like = result["data"]["current_condition"][0]["FeelsLikeF"]
+	humidity = result["data"]["current_condition"][0]["humidity"]
+	desc = result["data"]["current_condition"][0]["weatherDesc"][0]["value"]
+	wind_speed = result["data"]["current_condition"][0]["windspeedMiles"]
+	wind_dir = direction[result["data"]["current_condition"][0]["winddir16Point"]]
+
+	output = "It feels like #{feels_like} degrees with humidity of #{humidity} percent.  The conditions are #{desc} and wind is traveling at #{wind_speed} miles per hour #{wind_dir}."
+	speech = Speech.new(output)
+	speech.speak
+	# `say #{output}`
 end
 
 main
 
-# if test.nil?
-# 	puts "It was nil!"
-# else
-# 	puts test
-# end
-
-# resp = open(url).read
-
-# result = JSON.parse(resp)
-# puts result
 # test = "boop"
 # `say #{test}`
